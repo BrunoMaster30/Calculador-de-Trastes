ffc2731a26b454b9f6fc1693de910d8364d3fdcd
Bruno Ferreira Nishiya <58642490+BrunoMaster30@users.noreply.github.com>
2026-05-27 15:07:43 -0300

    Bugfix: Backend consertado e funcional

--- calctrast/src/service.clj
+++ calctrast/src/service.clj
@@ -1,7 +1,7 @@
 (ns service)
 
 (defn fret-position [scale n]
-  (/ scale (- (Math/pow 2 (/ n 12.0)) 1)))
+  (* scale (- 1 (/ 1 (Math/pow 2 (/ n 12.0))))))
 
 (defn calculate [scale frets]
   (map #(fret-position scale %) (range 1 (inc frets))))
